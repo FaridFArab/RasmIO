4a89f96cd80648d761c0ebe6cc01143ad89df951
PsychoFarid <faridarab4218@gmail.com>
2020-11-16 22:31:03 +0330

    Add config.ini file and separate main method.

---
+++ rasm.io.crawler/main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+if __name__ == '__main__':
+    print("Start Crawling With <ConfigName>")
+
+    # TODO Main Crawling
+
+    print("End Crawling With <ConfigName>")
